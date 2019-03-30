b561e57bdaad555fe0c5ff2d45cdedb4ada84be3
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-03-29 22:38:31 -0400

    Updating image to one that works

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function handleSubmit(){
-    const xhttp = new XMLHttpRequest();
-    const data = {
-        "username": document.getElementById("username").value,
-        "password": document.getElementById("password").value,
-    }
-    xhttp.onload = function(){
-        console.log(this.responseText)
-    }
-    xhttp.open("POST", "http:homebaseapp.com/api/login", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(JSON.stringify(data));
-}
+// function handleSubmit(){
+//     const xhttp = new XMLHttpRequest();
+//     const data = {
+//         "username": document.getElementById("username").value,
+//         "password": document.getElementById("password").value,
+//     }
+//     xhttp.onload = function(){
+//         console.log(this.responseText)
+//     }
+//     xhttp.open("POST", "http:homebaseapp.com/api/login", true);
+//     xhttp.setRequestHeader("Content-type", "application/json");
+//     xhttp.send(JSON.stringify(data));
+// }
