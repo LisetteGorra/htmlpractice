630eb5d464f058c3c53d1e71ff0b32319f78aed8
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-03-30 12:08:58 -0400

    Used fetch to post encoded data

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+
+function submit(){
+var url = "https://cors-anywhere.herokuapp.com/http://careerdev-teaching-lujason2015.c9users.io/api";
+var data = {username: 'wah wah'};
+
+fetch(url, {
+  method: 'POST', // or 'PUT'
+  body: JSON.stringify(data), // data can be `string` or {object}!
+  headers:{
+    'Content-Type': 'application/json'
+  }
+}).then(res => res.json())
+.then(response => console.log('Success:', JSON.stringify(response)))
+.catch(error => console.error('Error:', error));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function handleSubmit(){
 //     const xhttp = new XMLHttpRequest();
 //     const data = {
